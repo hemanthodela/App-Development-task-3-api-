49a5f35c10f36838d7021cf95abccebfd4ac3828
hemanthodela <hemanthodela5@gmail.com>
2026-06-03 11:52:35 +0530

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,15 +1,17 @@
 package com.makeeasy.retrofitfirst;
 
 public class modelclass {
-    int userId , id;
-    String title,body;
 
-    public modelclass(int userId,int id,String titel,String body) {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public modelclass(int userId, int id, String title, String body) {
         this.userId = userId;
         this.id = id;
         this.title = title;
         this.body = body;
-
     }
 
     public int getUserId() {
@@ -19,7 +21,28 @@ public class modelclass {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 }
